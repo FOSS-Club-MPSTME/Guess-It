f325f01b3f32d8883a4b16358e807688633d37de
Aakarsh Singh <singhaakarsh1506@gmail.com>
2026-07-11 14:09:25 +0530

    Flag - Guesser Routing Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LogoGuesser from "./pages/LogoGuesser";
+import FlagGuesser from "./pages/FlagGuesser";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/logo-guesser" element={<LogoGuesser />} />
+        <Route path="/flag-guesser" element={<FlagGuesser />} />
       </Routes>
     </BrowserRouter>
   );
